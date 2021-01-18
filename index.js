90be07cf1b7758a7402d4faa8a876a8197024ff5
r6hk416 <r6hk416@gmail.com>
2021-01-18 17:30:55 +0900

    add

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const Discord = require("discord.js")
 const intent_list = new Discord.Intents(["GUILD_MEMBERS", "GUILD_MESSAGES", "GUILDS", "GUILD_INVITES"])
 const client = new Discord.Client({ ws: { intents: intent_list } })
-const moment = require("moment")
-require("moment-duration-format")
-const momenttz = require("moment-timezone")
 const token = process.argv.length == 2 ? process.env.token : "" // heroku를 사용하지 않을꺼라면 const token = "디스코드 봇 토큰" 으로 바꿔주세요.
 const welcomeChannelName = "안녕하세요" // 입장 시 환영메시지를 전송 할 채널의 이름을 입력하세요.
 const byeChannelName = "안녕히가세요" // 퇴장 시 메시지를 전송 할 채널의 이름을 입력하세요.
